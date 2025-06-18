3948b241e00c3cebec45265b0a008bd84952d95d
boyra <b9892213@gmail.com>
2025-06-18 13:24:12 +0300

    import bot token and use it

--- apps/bot/src/bot.ts
+++ apps/bot/src/bot.ts
@@ -1,3 +1,4 @@
 import { Bot } from "grammy";
+import { BOT_TOKEN } from "@animman/bot/config";
 
-export default new Bot("8173274156:AAGJWueLO-Sa5F2K7Uf8gWG6QJ4P6UWKIbk");
+export default new Bot(BOT_TOKEN);
