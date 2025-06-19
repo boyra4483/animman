4b0372d9cc28cfa07913cf35b0a08c0272b50a09
boyra <b9892213@gmail.com>
2025-06-19 20:48:48 +0300

    use alias for importing

--- apps/tma/src/index.tsx
+++ apps/tma/src/index.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@animman/tma/app/App";
+import "@animman/tma/app/styles/index.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
