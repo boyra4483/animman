562b2ec5cd32f81eda2136d12a9c627ca5eacc1b
boyra <b9892213@gmail.com>
2025-06-21 18:14:44 +0300

    initial onboarding route config

---
+++ apps/tma/src/app/routes/onboarding.tsx
@@ -0,0 +1,7 @@
+import { RouteObject } from "react-router";
+import Onboarding from "@animman/tma/pages/onboarding/ui/Onboarding";
+
+export const onboardingRoute: RouteObject = {
+  path: "/onboarding",
+  element: <Onboarding />,
+};
