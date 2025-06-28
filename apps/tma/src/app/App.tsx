39ea04f32b38e5381d001cc725649bb36120fff1
boyra <b9892213@gmail.com>
2025-06-28 21:38:52 +0300

    remove importing scss and move it to app/App.tsx file

--- apps/tma/src/app/App.tsx
+++ apps/tma/src/app/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router";
 import router from "@animman/tma/app/router/router";
+import "@animman/tma/app/styles/index.scss";
 
 export default function App() {
   return <RouterProvider router={router} />;
