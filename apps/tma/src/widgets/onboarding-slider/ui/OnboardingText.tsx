a6af8f03d1f50644ef45521d6fd89ca625345752
boyra <b9892213@gmail.com>
2025-06-30 17:35:41 +0300

    create "texts" var and add dynamism

--- apps/tma/src/widgets/onboarding-slider/ui/OnboardingText.tsx
+++ apps/tma/src/widgets/onboarding-slider/ui/OnboardingText.tsx
@@ -1,15 +1,28 @@
 import styles from "@animman/tma/widgets/onboarding-slider/ui/onboardingText.scss";
+import { Text } from "@animman/tma/widgets/onboarding-slider/model/text";
+const texts: Text[] = [
+  {
+    title: "The biggest international and local film streaming",
+    description:
+      "Semper in cursus magna et eu varius nunc adipiscing. Elementum justo,laoreet id sem semper parturient.",
+  },
+  {
+    title: "Offers ad-free viewing of high quality",
+    description:
+      "Semper in cursus magna et eu varius nunc adipiscing. Elementum justo,laoreet id sem semper parturient.",
+  },
+  {
+    title: "Our service brings together your favorite series",
+    description:
+      "Semper in cursus magna et eu varius nunc adipiscing. Elementum justo,laoreet id sem semper parturient.",
+  },
+];
 
-export default function OnboardingText() {
+export default function OnboardingText({ step }: { step: number }) {
   return (
     <div className={styles.texts}>
-      <p className={styles.title}>
-        The biggest international and local film streaming
-      </p>
-      <p className={styles["body-text"]}>
-        Semper in cursus magna et eu varius nunc adipiscing. Elementum justo,
-        laoreet id sem semper parturient.
-      </p>
+      <p className={styles.title}>{texts[step].title}</p>
+      <p className={styles["body-text"]}>{texts[step].description}</p>
     </div>
   );
 }
