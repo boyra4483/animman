03ca14a89195c778b6619076c5ad31f7449f1f40
boyra <b9892213@gmail.com>
2025-06-30 14:38:03 +0300

    refactoring
    width/height
    change px unit to rem
    
    delete commented rules

---
+++ apps/tma/src/widgets/onboarding-slider/ui/OnboardingSlides.tsx
@@ -0,0 +1,37 @@
+import styles from "@animman/tma/widgets/onboarding-slider/ui/onboardingSlides.scss";
+import onboardingImg from "@animman/tma/shared/assets/images/pictures/onboarding.jpg";
+import { Images } from "@animman/tma/shared/types/image";
+
+interface OnboardingSlidesProps {
+  slide: number;
+}
+
+export default function OnboardingSlides({ slide }: OnboardingSlidesProps) {
+  const images: Images = [
+    {
+      src: onboardingImg,
+      alt: "woman with phone",
+      width: 375,
+      height: 421,
+    },
+    {
+      src: onboardingImg,
+      alt: "woman with phone",
+      width: 375,
+      height: 421,
+    },
+    {
+      src: onboardingImg,
+      alt: "woman with phone",
+      width: 375,
+      height: 421,
+    },
+  ];
+  return (
+    <img
+      className={styles.img}
+      src={images[slide].src}
+      alt={images[slide].alt}
+    />
+  );
+}
