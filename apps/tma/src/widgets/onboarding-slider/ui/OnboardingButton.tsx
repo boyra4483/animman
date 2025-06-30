24455d649605f3aa6e85c64d8e0db9cc31dccf80
boyra <b9892213@gmail.com>
2025-06-30 17:46:28 +0300

    create interface and add "onClick" handler

--- apps/tma/src/widgets/onboarding-slider/ui/OnboardingButton.tsx
+++ apps/tma/src/widgets/onboarding-slider/ui/OnboardingButton.tsx
@@ -1,15 +1,19 @@
 import styles from "@animman/tma/widgets/onboarding-slider/ui/onboardingButton.scss";
 import arrow from "@animman/tma/shared/assets/images/icons/arrow.svg";
 
+interface OnboardingButtonProps {
+  children: React.ReactElement;
+  onClick: () => void;
+}
+
 export default function OnboardingButton({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+  onClick,
+}: OnboardingButtonProps) {
   return (
     <div className={styles["button-wrapper"]}>
       {children}
-      <div className={styles.btn}>
+      <div className={styles.btn} onClick={onClick}>
         <img className={styles["arrow"]} src={arrow} alt="arrow" />
       </div>
     </div>
