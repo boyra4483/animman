797e9551c13dab1658d5cc52f9c6cc5aebe4bcf6
boyra <b9892213@gmail.com>
2025-06-30 14:43:44 +0300

    move "images" var out of function component

--- apps/tma/src/widgets/onboarding-slider/ui/OnboardingSlides.tsx
+++ apps/tma/src/widgets/onboarding-slider/ui/OnboardingSlides.tsx
@@ -1,28 +1,28 @@
 import styles from "@animman/tma/widgets/onboarding-slider/ui/onboardingSlides.scss";
 import onboardingImg from "@animman/tma/shared/assets/images/pictures/onboarding.jpg";
 import { Images } from "@animman/tma/shared/types/image";
+const images: Images = [
+  {
+    src: onboardingImg,
+    alt: "woman with phone",
+    width: 375,
+    height: 421,
+  },
+  {
+    src: onboardingImg,
+    alt: "woman with phone",
+    width: 375,
+    height: 421,
+  },
+  {
+    src: onboardingImg,
+    alt: "woman with phone",
+    width: 375,
+    height: 421,
+  },
+];
 
 export default function OnboardingSlides({ slide }: { slide: number }) {
-  const images: Images = [
-    {
-      src: onboardingImg,
-      alt: "woman with phone",
-      width: 375,
-      height: 421,
-    },
-    {
-      src: onboardingImg,
-      alt: "woman with phone",
-      width: 375,
-      height: 421,
-    },
-    {
-      src: onboardingImg,
-      alt: "woman with phone",
-      width: 375,
-      height: 421,
-    },
-  ];
   return (
     <img
       className={styles.img}
