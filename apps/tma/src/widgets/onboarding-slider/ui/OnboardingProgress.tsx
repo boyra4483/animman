85518a412f10acf5e5c87912c9c5046cdf1be8ed
boyra <b9892213@gmail.com>
2025-06-30 15:46:05 +0300

    develop static version of "OnboardingProgress" component

---
+++ apps/tma/src/widgets/onboarding-slider/ui/OnboardingProgress.tsx
@@ -0,0 +1,34 @@
+import styles from "@animman/tma/widgets/onboarding-slider/ui/onboardingProgress.scss";
+
+interface OnboardingProgressProps {
+  step: number;
+  quantity: number;
+}
+
+export default function OnboardingProgress({
+  step,
+  quantity,
+}: OnboardingProgressProps) {
+  return (
+    <ul className={styles["progress"]}>{...getProgress(quantity, step)}</ul>
+  );
+}
+
+function getProgress(quantity: number, activeStep: number) {
+  const steps: React.ReactElement<HTMLLIElement>[] = [];
+
+  for (let i = 0; i < quantity; i++) {
+    const isActive = i === activeStep;
+    const stepElement = (
+      <li
+        key={i}
+        className={
+          isActive ? `${styles["step"]} ${styles["active"]}` : styles["step"]
+        }
+      ></li>
+    );
+
+    steps.push(stepElement);
+  }
+  return steps;
+}
