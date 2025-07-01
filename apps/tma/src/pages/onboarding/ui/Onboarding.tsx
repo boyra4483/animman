22d0182902afa4bb7a82fc08e38a6439757b79dd
boyra <b9892213@gmail.com>
2025-07-01 16:49:14 +0300

    import "OnboardingSlider" and return it

---
+++ apps/tma/src/pages/onboarding/ui/Onboarding.tsx
@@ -0,0 +1,8 @@
+import OnboardingSlider from "@animman/tma/widgets/onboarding-slider";
+export default function Onboarding() {
+  return (
+    <div>
+      <OnboardingSlider />;
+    </div>
+  );
+}
