8a601daf503e8bcae5e826e875b8453e233fde31
boyra <b9892213@gmail.com>
2025-07-01 18:36:36 +0300

    initialize intinitial server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,3 +1,13 @@
-export function helloServer() {
-  console.log("Hello from server!");
-}
+import e from "express";
+
+const app = e();
+const PORT = process.env.PORT || 3030;
+
+app.get("/", (req, res) => {
+  console.log(req);
+  res.send({ message: "Animman backend is working 🚀" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
