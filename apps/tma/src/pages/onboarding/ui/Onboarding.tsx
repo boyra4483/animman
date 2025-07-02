a0e2bfd5a47afcaef1bd1c3811fda589be83dc90
boyra <b9892213@gmail.com>
2025-07-02 16:33:36 +0300

    remove ; char

--- apps/tma/src/pages/onboarding/ui/Onboarding.tsx
+++ apps/tma/src/pages/onboarding/ui/Onboarding.tsx
@@ -2,7 +2,7 @@ import OnboardingSlider from "@animman/tma/widgets/onboarding-slider";
 export default function Onboarding() {
   return (
     <div>
-      <OnboardingSlider />;
+      <OnboardingSlider />
     </div>
   );
 }
