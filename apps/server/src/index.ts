a2145fb46fd5892dce2a3252b63691902cfde9a3
boyra <b9892213@gmail.com>
2025-07-02 14:56:41 +0300

    import primsa instance and return its users as res

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,11 +1,12 @@
 import e from "express";
+import prisma from "@animman/server/prisma";
 
 const app = e();
 const PORT = process.env.PORT || 3030;
 
-app.get("/", (req, res) => {
-  console.log(req);
-  res.send({ message: "Animman backend is working 🚀" });
+app.get("/", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
 });
 
 app.listen(PORT, () => {
