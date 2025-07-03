868e3b96fa8ced8fb4570addf1528c20014775be
boyra <b9892213@gmail.com>
2025-07-03 17:09:40 +0300

    import files - jwt - isValid - parse - cookieParser

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,5 +1,8 @@
 import e from "express";
 import prisma from "@animman/server/prisma";
+import { isValid, parse } from "@telegram-apps/init-data-node";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
 const app = e();
 const PORT = process.env.PORT || 3030;
