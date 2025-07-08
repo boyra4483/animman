34ce6b17f8f48345bc46cb35107803fbbca3e6f9
boyra <b9892213@gmail.com>
2025-07-08 15:18:12 +0300

    create endpoint and export router as default
    import userController and apply it to the "/auth/verify" post endpoint

--- apps/server/src/router.ts
+++ apps/server/src/router.ts
@@ -1,3 +1,7 @@
 import { Router } from "express";
+import userController from "@animman/server/UserController";
 
 const router = Router();
+router.post("/auth/verify", userController.auth);
+
+export default router;
