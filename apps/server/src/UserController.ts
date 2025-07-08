9d61af009ab2166e020cbeefee7805559d1c4f53
boyra <b9892213@gmail.com>
2025-07-08 15:37:38 +0300

    create "UserController" class it has the 2 functions: async auth fn and initDataVerify fn.
    initDataVerify
    
    it verifies on authenticity "initData" that comes from Telegram and has 2 checking logic.
    
    1. if "initData" doesn't come (it is undefined) the fn throw the error
    2. second checking logic verify "initData" via special "isValid" fn and if "initData"
    isn't authentic it also throw the error
    
    after all that verifications and no one throws error the initDataVerify fn return the parsed
    "initData" via special "parse" fn to read the info that is in it.
    
    async auth
    
    if initDataVerify throws the error async auth cathes it and send 400 status code.
    otherwise it follows this steps:
    1. it creates or updates user via prisma client.
    2. it creates jwt and keeps it in cockie that will be in response
    3. it sends response that contains the authorized user and a message

---
+++ apps/server/src/UserController.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from "express";
+import { parse, isValid } from "@telegram-apps/init-data-node";
+import jwt from "jsonwebtoken";
+import prisma from "@animman/server/prisma";
+
+class UserController {
+  async auth(req: Request, res: Response) {
+    const { initData } = req.body as { initData: string };
+
+    try {
+      const userTgData = this.initDataVerify(initData);
+      const user = await prisma.user.upsert({
+        where: { telegramId: userTgData.user?.id.toString() },
+        update: {
+          username: userTgData.user?.username,
+          firstName: userTgData.user?.first_name,
+          lastName: userTgData.user?.last_name,
+          photoUrl: userTgData.user?.photo_url,
+        },
+        create: {
+          telegramId: userTgData.user?.id.toString()!,
+          username: userTgData.user?.username,
+          firstName: userTgData.user?.first_name,
+          lastName: userTgData.user?.last_name,
+          photoUrl: userTgData.user?.photo_url,
+        },
+      });
+
+      const token = jwt.sign(
+        {
+          userId: user.id,
+        },
+        process.env.JWT_SECRET!,
+        { expiresIn: "7d" }
+      );
+
+      res.cookie("token", token, {
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+      });
+      res.json({
+        message: "Authorized",
+        user,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({
+        error: "Invalid initData",
+      });
+    }
+  }
+  initDataVerify(initData: string | undefined) {
+    if (initData === undefined) {
+      throw new Error(
+        "you did not connect to the animman using Telegram. Please, use animman via telegram"
+      );
+    }
+    if (!isValid(initData, process.env.JWT_SECRET!)) {
+      throw new Error("invalid initData");
+    }
+    return parse(initData);
+  }
+}
+
+export default new UserController();
