a949b4e3e4bb107ba9f0c0ec44c4f970af5fa669
boyra <b9892213@gmail.com>
2025-07-15 18:38:23 +0300

    wrapp out the img in a <div>

--- apps/tma/src/widgets/onboarding-slider/ui/OnboardingSlides.tsx
+++ apps/tma/src/widgets/onboarding-slider/ui/OnboardingSlides.tsx
@@ -2,32 +2,34 @@ import styles from "@animman/tma/widgets/onboarding-slider/ui/onboardingSlides.s
 import onboardingImg from "@animman/tma/shared/assets/images/pictures/onboarding.jpg";
 import { Images } from "@animman/tma/shared/types/image";
 const images: Images = [
-  {
-    src: onboardingImg,
-    alt: "woman with phone",
-    width: 425,
-    height: 421,
-  },
-  {
-    src: onboardingImg,
-    alt: "woman with phone",
-    width: 425,
-    height: 421,
-  },
-  {
-    src: onboardingImg,
-    alt: "woman with phone",
-    width: 425,
-    height: 421,
-  },
+	{
+		src: onboardingImg,
+		alt: "woman with phone",
+		width: 425,
+		height: 421
+	},
+	{
+		src: onboardingImg,
+		alt: "woman with phone",
+		width: 425,
+		height: 421
+	},
+	{
+		src: onboardingImg,
+		alt: "woman with phone",
+		width: 425,
+		height: 421
+	}
 ];
 
 export default function OnboardingSlides({ slide }: { slide: number }) {
-  return (
-    <img
-      className={styles.img}
-      src={images[slide].src}
-      alt={images[slide].alt}
-    />
-  );
+	return (
+		<div className={styles["img-wrapper"]}>
+			<img
+				className={styles.img}
+				src={images[slide].src}
+				alt={images[slide].alt}
+			/>
+		</div>
+	);
 }
