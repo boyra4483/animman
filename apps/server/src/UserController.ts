840e180fb48e57d6885b53ab2c9e4c6e7a86285e
boyra <b9892213@gmail.com>
2025-07-18 16:33:47 +0300

    add isAuth fn that checks if there is user in DB or not;

--- apps/server/src/UserController.ts
+++ apps/server/src/UserController.ts
@@ -5,59 +5,86 @@ import prisma from "@animman/server/prisma";
 import InitDataError from "@animman/server/errors/initDataError";
 
 class UserController {
-  async auth(req: Request, res: Response) {
-    const { initData } = req.body as { initData: string };
+	async auth(req: Request, res: Response) {
+		const { initData } = req.body as { initData: string };
 
-    try {
-      const userTgData = this.initDataVerify(initData);
-      const user = await prisma.user.upsert({
-        where: { telegramId: userTgData.user?.id.toString() },
-        update: {
-          username: userTgData.user?.username,
-          firstName: userTgData.user?.first_name,
-          lastName: userTgData.user?.last_name,
-          photoUrl: userTgData.user?.photo_url,
-        },
-        create: {
-          telegramId: userTgData.user?.id.toString()!,
-          username: userTgData.user?.username,
-          firstName: userTgData.user?.first_name,
-          lastName: userTgData.user?.last_name,
-          photoUrl: userTgData.user?.photo_url,
-        },
-      });
+		try {
+			const userTgData = this.initDataVerify(initData);
+			const user = await prisma.user.upsert({
+				where: { telegramId: userTgData.user?.id.toString() },
+				update: {
+					username: userTgData.user?.username,
+					firstName: userTgData.user?.first_name,
+					lastName: userTgData.user?.last_name,
+					photoUrl: userTgData.user?.photo_url
+				},
+				create: {
+					telegramId: userTgData.user?.id.toString()!,
+					username: userTgData.user?.username,
+					firstName: userTgData.user?.first_name,
+					lastName: userTgData.user?.last_name,
+					photoUrl: userTgData.user?.photo_url
+				}
+			});
 
-      const token = jwt.sign(
-        {
-          userId: user.id,
-        },
-        process.env.JWT_SECRET!,
-        { expiresIn: "7d" }
-      );
+			const token = jwt.sign(
+				{
+					userId: user.id
+				},
+				process.env.JWT_SECRET!,
+				{ expiresIn: "7d" }
+			);
 
-      res.cookie("token", token, {
-        httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV === "production",
-      });
-      res.json({
-        message: "Authorized",
-        user,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(400).json({
-        error: "Invalid initData",
-      });
-    }
-  }
-  initDataVerify(initData: string | undefined) {
-    if (initData === undefined || !isValid(initData, process.env.JWT_SECRET!)) {
-      throw new InitDataError();
-    }
+			res.cookie("token", token, {
+				httpOnly: true,
+				sameSite: "strict",
+				secure: process.env.NODE_ENV === "production"
+			});
+			res.json({
+				message: "Authorized",
+				user
+			});
+		} catch (error) {
+			console.log(error);
+			res.status(400).json({
+				error: "Invalid initData"
+			});
+		}
+	}
+	initDataVerify(initData: string | undefined) {
+		if (initData === undefined || !isValid(initData, process.env.JWT_SECRET!)) {
+			throw new InitDataError();
+		}
 
-    return parse(initData);
-  }
+		return parse(initData);
+	}
+	isAuth(req: Request, res: Response) {
+		const { initData } = req.body as { initData: string };
+
+		try {
+			const userTgData = this.initDataVerify(initData);
+			const userDB = prisma.user.findFirst({
+				where: {
+					telegramId: userTgData.user?.id.toString()
+				}
+			});
+
+			if (userDB === null) {
+				res.status(401).json({
+					user: userDB
+				});
+				return;
+			}
+			res.status(200).json({
+				user: userDB
+			});
+		} catch (err) {
+			console.log(err);
+			res.status(400).json({
+				error: "Invalid initData"
+			});
+		}
+	}
 }
 
 export default new UserController();
