784c0cf23da29dc3a5103f2c26fae74327b1d960
boyra <b9892213@gmail.com>
2025-07-18 16:58:51 +0300

    create fn that redirect in depending on whether user is auth or not
    import telegram SDK to send initData receved from SDK and useNavigate
    hook that redirects in depending on whether user is auth or not.

--- apps/tma/src/app/route-loaders/rootLoader.ts
+++ apps/tma/src/app/route-loaders/rootLoader.ts
@@ -1,1 +1,17 @@
-export default async function rootLoader() {}
+import tg from "@twa-dev/sdk";
+import { useNavigate } from "react-router";
+
+export default async function rootLoader() {
+	const navigate = useNavigate();
+
+	const res = await fetch("http://localhost:3030/user/isAuth ", {
+		method: "POST",
+		body: JSON.stringify({
+			initData: tg.initData
+		})
+	});
+	const user = await res.json();
+
+	if (!user.userData) return navigate("/onboarding");
+	return navigate("/");
+}
