b9e53ee5d2ecf05f3ff1278036ad9b7012ab2472
boyra <b9892213@gmail.com>
2025-07-18 14:26:48 +0300

    change the texts to be displayed on the onboarding page

--- apps/tma/src/widgets/onboarding-slider/ui/OnboardingText.tsx
+++ apps/tma/src/widgets/onboarding-slider/ui/OnboardingText.tsx
@@ -1,26 +1,26 @@
 import styles from "@animman/tma/widgets/onboarding-slider/ui/onboardingText.scss";
 import { Text } from "@animman/tma/widgets/onboarding-slider/model/text";
 const texts: Text[] = [
-  {
-    title: "Добро пожаловать в Animman! ",
-    description:
-      "Здесь ты найдёшь всё об аниме: от описаний и рейтингов до новых релизов.",
-  },
-  {
-    title: "Animman – твой гид по миру аниме",
-    description: "Ищи, сохраняй и узнавай больше о своих любимых тайтлах.",
-  },
-  {
-    title: "Найди своё новое любимое аниме уже сегодня",
-    description: "Твои любимые тайтлы и свежие релизы – в одном месте.",
-  },
+	{
+		title: "Добро пожаловать в Animman! ",
+		description:
+			"Здесь ты найдёшь всё об аниме: от новостей и рейтингов до новых релизов."
+	},
+	{
+		title: "Animman – твой гид по миру аниме",
+		description: "Ищи, сохраняй и узнавай больше о своих любимых тайтлах."
+	},
+	{
+		title: "Найди своё новое любимое аниме уже в Animman",
+		description: "Твои любимые тайтлы и свежие релизы – в одном месте."
+	}
 ];
 
 export default function OnboardingText({ step }: { step: number }) {
-  return (
-    <div className={styles.texts}>
-      <p className={styles.title}>{texts[step].title}</p>
-      <p className={styles["body-text"]}>{texts[step].description}</p>
-    </div>
-  );
+	return (
+		<div className={styles.texts}>
+			<p className={styles.title}>{texts[step].title}</p>
+			<p className={styles["body-text"]}>{texts[step].description}</p>
+		</div>
+	);
 }
