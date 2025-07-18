212f7066524897b1ab634d444fee901ce9d320ce
boyra <b9892213@gmail.com>
2025-07-18 16:20:08 +0300

    apply root loader to root route

--- apps/tma/src/app/routes/root.tsx
+++ apps/tma/src/app/routes/root.tsx
@@ -1,8 +1,10 @@
 import { type RouteObject } from "react-router";
+import rootLoader from "../route-loaders/rootLoader";
 
 const rootRout: RouteObject = {
-  path: "/",
-  element: <h1>hello</h1>,
+	path: "/",
+	element: <h1>hello</h1>,
+	loader: rootLoader
 };
 
 export default rootRout;
