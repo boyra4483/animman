48f812b6b46112a49506fb7448fbcda2a2effa08
boyra <b9892213@gmail.com>
2025-07-19 15:54:50 +0300

    add console.log for logging

--- apps/tma/src/app/route-loaders/rootLoader.ts
+++ apps/tma/src/app/route-loaders/rootLoader.ts
@@ -2,6 +2,7 @@ import tg from "@twa-dev/sdk";
 import { redirect } from "react-router";
 
 export default async function rootLoader() {
+	console.log(tg.initData);
 	const res = await fetch("http://localhost:3030/user/isAuth", {
 		method: "POST",
 		body: JSON.stringify({
