cac336d6d168d0e368ec5df4b8e689667b0bc5b9
boyra <b9892213@gmail.com>
2025-07-20 15:18:55 +0300

    apply cors middleware to express server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,8 @@
 import e from "express";
 import cookieParser from "cookie-parser";
 import router from "@animman/server/router";
+import cors from "cors";
+import "dotenv/config";
 
 const app = e();
 const PORT = process.env.PORT || 3030;
@@ -8,6 +10,12 @@ const PORT = process.env.PORT || 3030;
 app.use(e.json());
 app.use(cookieParser());
 app.use("/user", router);
+app.use(
+	cors({
+		origin: process.env.TMA_URL,
+		credentials: true
+	})
+);
 
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
