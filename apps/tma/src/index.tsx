05a66da42238778069c6ce53382f4585256ce78c
boyra <b9892213@gmail.com>
2025-07-28 12:02:42 +0300

    run eruda for mobile debugging

--- apps/tma/src/index.tsx
+++ apps/tma/src/index.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@animman/tma/app/App";
+import eruda from "eruda";
+eruda.init();
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
