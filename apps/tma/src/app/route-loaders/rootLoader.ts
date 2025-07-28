54b501abedc8ffcdc8b205913afe174c2599ecba
boyra <b9892213@gmail.com>
2025-07-28 12:36:18 +0300

    remove unnecessary import and return redirect response

--- apps/tma/src/app/route-loaders/rootLoader.ts
+++ apps/tma/src/app/route-loaders/rootLoader.ts
@@ -1,5 +1,5 @@
 import tg from "@twa-dev/sdk";
-import { data, redirect } from "react-router";
+import { redirect } from "react-router";
 import isAuth from "@animman/tma/shared/config/axios/config/isAuth";
 import axios from "axios";
 
@@ -7,9 +7,6 @@ export default async function rootLoader() {
 	console.log(tg.initData);
 	const res = await axios(isAuth);
 
-	if (res.statusText == "OK") {
-		redirect("/auth");
-	} else {
-		redirect("/onboarding");
-	}
+	if (res.statusText == "OK") return redirect("/auth");
+	return redirect("/onboarding");
 }
