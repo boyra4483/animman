acf40634c39ce1b93479c47c9a1766d616a38fcc
boyra <b9892213@gmail.com>
2025-07-31 14:18:17 +0300

    create user interface consisting of all props from db user model

---
+++ packages/shared/src/types/user.ts
@@ -0,0 +1,10 @@
+interface User {
+	id: string;
+	telegramId: string;
+	username: string | null;
+	firstName: string | null;
+	lastName: string | null;
+	photoUrl: string | null;
+	createdAt: Date;
+	updatedAt: Date;
+}
