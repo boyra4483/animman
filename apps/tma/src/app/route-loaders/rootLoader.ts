dd833c8f456331c2879a1a4b2bddf1681ac846c5
boyra <b9892213@gmail.com>
2025-08-07 14:12:11 +0300

    type the axios response and change its name

--- apps/tma/src/app/route-loaders/rootLoader.ts
+++ apps/tma/src/app/route-loaders/rootLoader.ts
@@ -2,11 +2,12 @@ import tg from "@twa-dev/sdk";
 import { redirect } from "react-router";
 import isAuth from "@animman/tma/shared/config/axios/config/isAuth";
 import axios from "axios";
+import User from "@animman/shared/src/types/user";
 
 export default async function rootLoader() {
 	console.log(tg.initData);
-	const res = await axios(isAuth);
+	const user = await axios<any, User>(isAuth);
 
-	if (res.statusText == "OK") return redirect("/auth");
+	if (user.userData !== null) return redirect("/auth");
 	return redirect("/onboarding");
 }
