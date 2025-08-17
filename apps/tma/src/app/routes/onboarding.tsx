7f33a62b2643fb027d8d550489f182610d75c6b4
boyra <b9892213@gmail.com>
2025-08-17 15:37:51 +0300

    export route as default instead of named import

--- apps/tma/src/app/routes/onboarding.tsx
+++ apps/tma/src/app/routes/onboarding.tsx
@@ -1,7 +1,9 @@
 import { RouteObject } from "react-router";
 import Onboarding from "@animman/tma/pages/onboarding/ui/Onboarding";
 
-export const onboardingRoute: RouteObject = {
-  path: "/onboarding",
-  element: <Onboarding />,
+const onboardingRoute: RouteObject = {
+	path: "/onboarding",
+	element: <Onboarding />
 };
+
+export default onboardingRoute;
