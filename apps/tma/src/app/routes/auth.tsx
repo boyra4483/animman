74df2718a8ce27366a8315d256a8464319e2faee
boyra <b9892213@gmail.com>
2025-08-17 15:40:17 +0300

    create route for auth

---
+++ apps/tma/src/app/routes/auth.tsx
@@ -0,0 +1,9 @@
+import { RouteObject } from "react-router";
+import Auth from "@animman/tma/pages/auth/ui/Auth";
+
+const authRoute: RouteObject = {
+	path: "/auth",
+	element: <Auth />
+};
+
+export default authRoute;
