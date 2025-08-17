45da90a308c7edf61f3185644552d41d10fbcda0
boyra <b9892213@gmail.com>
2025-08-17 15:37:00 +0300

    import auth, logn and register routes

--- apps/tma/src/app/router/router.ts
+++ apps/tma/src/app/router/router.ts
@@ -1,7 +1,16 @@
 import { createBrowserRouter } from "react-router";
 import rootRout from "@animman/tma/app/routes/root";
-import { onboardingRoute } from "../routes/onboarding";
+import onboardingRoute from "@animman/tma/app/routes/onboarding";
+import authRoute from "@animman/tma/app/routes/auth";
+import loginRoute from "../routes/login";
+import registerRoute from "../routes/register";
 
-const router = createBrowserRouter([rootRout, onboardingRoute]);
+const router = createBrowserRouter([
+	rootRout,
+	onboardingRoute,
+	authRoute,
+	loginRoute,
+	registerRoute
+]);
 
 export default router;
