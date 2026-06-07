8a69b8799d989c46ffd133290ad9c0118059200d
boyra <b9892213@gmail.com>
2026-06-07 05:49:54 +0300

    create simple server to deploy it on render

--- apps/bot/src/bot.ts
+++ apps/bot/src/bot.ts
@@ -1,6 +1,19 @@
 import { Bot } from "grammy";
 import { BOT_TOKEN } from "@animman/bot/config";
 import { startCommand } from "@animman/bot/commands/start";
+import http from "http";
+
+const server = http.createServer((req, res) => {
+	return "server is working";
+});
+
+server.listen(
+	process.env.SERVER_PORT,
+	Number(process.env.SERVER_HOST_NAME),
+	() => {
+		console.log("server is working");
+	}
+);
 
 export const bot = new Bot(BOT_TOKEN);
 
